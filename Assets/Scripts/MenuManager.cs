7ba31542182155619f8cecc3173cec158525a4b7
Ojax <ojalrak@gmail.com>
2025-07-23 22:06:11 +0100

    added a background and working on menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,8 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    public void GoToMenu()
-    {
-        SceneManager.LoadScene("Scene1"); // or whatever your menu scene is named
-    }
+   public void StartGame()
+{
+    SceneManager.LoadScene("Scene1"); // Or your first level scene
+}
+
 }
