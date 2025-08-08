6efcdfdf350a09a2c6477351f6c612d17221b153
Ojax <ojalrak@gmail.com>
2025-08-08 13:31:58 +0100

    added buttons taking you to each level in main menu

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -3,9 +3,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-   public void StartGame()
-{
-    SceneManager.LoadScene("Scene1"); // Or your first level scene
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Scene1"); // Or your first level scene
+    }
+
+    public void GoToScene1()
+    {
+        SceneManager.LoadScene("Scene1");//for ur scene1 
+    }
+
+    public void GoToScene2()
+    {
+        SceneManager.LoadScene("Scene2");//for ur scene2
+    }
+    public void GoToScene3()
+    {
+        SceneManager.LoadScene("Scene3");//for ur scene2
+    }
+
+    public void GoToGameOver()
+    {
+        SceneManager.LoadScene("GameOver");//for ur game over scene
+    }
+
+    public void GoToWin()
+    {
+        SceneManager.LoadScene("Win");//for ur win scene
+    }
 }
 
-}
+
+
